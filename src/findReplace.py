bd31d4129c350ca25101f47ac0f98be9460c8850
Devin Sidhu <devinsidhu24@gmail.com>
2022-10-18 19:03:48 +0100

    Initial commit

--- src/findReplace.py
+++ src/findReplace.py
@@ -1,6 +1,14 @@
-from PyQt6.QtWidgets import QDialog, QLineEdit, QPushButton, QGridLayout
+from PyQt6.QtWidgets import QDialog, QLineEdit, QPushButton, QGridLayout, QMessageBox, QStyle
 
-def find():
+
+""" 
+Top-level function that creates popup and provides a conduit via which to access the other functions in this module.
+
+PARAMETERS:
+    text - the text being examined.
+    editor - the QTextEdit object for the code editor (So we can highlight found text).
+"""
+def find(text, editor):
 
     popup = QDialog()
     popup.setFixedSize(300, 100)
@@ -16,11 +24,14 @@ def find():
     findBox.setFixedSize(120, 20)
     findBox.setStyleSheet("background-color: #151821; border-style: none;")
     findBox.setPlaceholderText("Find")
+
+    instances = [] # 3D array - Stores cartesian coordinates of all found text instances (to be modified by __getInstances())
+    findBox.returnPressed.connect(lambda: __getInstances(findBox.text(), text, instances))
  
     nextInstance = QPushButton("Next", popup)
-    nextInstance.setFixedSize(50, 20)
+    nextInstance.setFixedSize(60, 20)
     prevInstance = QPushButton("Previous", popup)
-    prevInstance.setFixedSize(50, 20)
+    prevInstance.setFixedSize(60, 20)
 
     layout.addWidget(findBox, 0, 0)    
     layout.addWidget(nextInstance, 0, 1)
@@ -44,3 +55,56 @@ def find():
 
     popup.setLayout(layout)
     popup.exec()
+
+
+"""
+Gets instances of found text and highlights results
+
+PARAMETERS:
+    searchTerm - The text to search FOR.
+    inspectText - The text to search.
+    instances - The instances array
+    editor - the QTextEdit object for the code editor (So we can highlight text).
+
+"""
+def __getInstances(searchTerm, inspectText, instances, editor):
+
+    instances = [] # clear instances array in case instances from function's last execution are left in.
+    
+    position = [] # 2D array - Stores the position of 1 instance (cartesian coordinate of initial character & cartesian coordinate of final)
+    isDiff = False # Flag indicating whether text being examined is discrepant from searchTerm
+
+    inspectGrid = inspectText.split("\n") # Split text into 2D grid, ending at each newline. This allows us to get the coordinates of each character. 
+
+    for y in range(len(inspectGrid)):
+        for x in range(len(inspectGrid[y])):
+
+            if inspectGrid[y][x] == searchTerm[0]:
+
+                if len(searchTerm) > 1: # As we have established that the first characters are equal, if the searchTerm only has 1 character then we can conclude the scanning of this particular instance.
+
+                    for xOffset in range(1, len(searchTerm)): # Range starts at 1 as we have already established that the first characters are equal.
+                        try:
+                            if searchTerm[xOffset] != inspectGrid[y][x + xOffset]:
+                                isDiff = True
+                                break
+                        except IndexError: # This occurs when (x + xOffset) is "off of the end" of the line being examined i.e x + xOffset > len(inspectGrid[y]). In which case the searchTerm is not present here, and we move on.
+                            isDiff = True
+                            break
+                
+                else:
+                    xOffset = 0 # There is no xOffset if searchTerm only has 1 character.
+            
+                if not isDiff:
+                    position = [[x, y],[x + xOffset, y]]
+                    instances.append(position)
+
+                isDiff = False
+
+    if instances == []:
+        msgBox = QMessageBox()
+        msgBox.setWindowTitle("BoothiumEdit")
+        msgBox.setText(f"Couldn't find '{searchTerm}'")
+        msgBox.exec()
+    else:
+        editor
--- src/main.py
+++ src/main.py
@@ -1,89 +1,88 @@
-# PyQt6 imports
 from PyQt6.QtWidgets import QApplication, QTextEdit, QMainWindow, QToolBar
 from PyQt6.QtGui import QAction, QFontMetrics, QKeySequence
 
-# Python standard lib imports
 import sys
 import platform
 import json
 
-# Local file imports
 import saving
 import findReplace
 
 
-# Initialize PyQt app
 app = QApplication([])
 
 
-# Getting file from command line arguments
-try:
-	fileName = sys.argv[1]
-except IndexError: # Handle user not providing a filename
-	sys.exit("ERROR: No filename specified")
+class Window(QMainWindow):
 
-try:
-	file = open(fileName, "r+")
-except FileNotFoundError: # Handle user providing nonexistent file
-	sys.exit("ERROR: File does not exist")
+    def __init__(self):
 
-fileText = file.read()
+        self.setWindowTitle("BoothiumEdit - " + fileNameNoPath)
+        self.setGeometry(100, 100, 1000, 1000)
+        self.setMinimumSize(500, 500)
 
 
-# Get name of file without rest of path
-if platform.system() == "Windows": # Account for windows using "\" as path seperator rather than "/"
-	pathSeperator = '\\' # Double backslash to escape the first backslash
-else:
-	pathSeperator = '/'
+        # Getting file from command line arguments
+        try:
+            fileName = sys.argv[1]
+        except IndexError: # Handle user not providing a filename
+            sys.exit("ERROR: No filename specified")
 
-lastPathSepIndex = fileName[::-1].index(pathSeperator) # Reverse file path to facilitate getting index of last path seperator 
-fileNameNoPath = ((fileName[::-1])[:lastPathSepIndex])[::-1] # get characters of reverse path that precede last path seperator, then reverse this to get them finally in the right order
+        try:
+            file = open(fileName, "r+")
+        except FileNotFoundError: # Handle user providing nonexistent file
+            sys.exit("ERROR: File does not exist")
 
-
-# Setup for window widget
-window = QMainWindow()
-window.setWindowTitle("BoothiumEdit - " + fileNameNoPath)
-window.setGeometry(100, 100, 1000, 1000)
-window.setMinimumSize(500, 500)
+        fileText = file.read()
 
 
-# Setup for code editor
-editor = QTextEdit()
-editor.setStyleSheet("""color: white; 
-						background-color: #484D5D; 
-						border-style: none; 
-						font: courier new; 
-						font-size: 10pt;""")
-editor.setTabStopDistance(QFontMetrics(editor.font()).horizontalAdvance(' ') * 4) # Changing tab size to 4 spaces 
-window.setCentralWidget(editor)
-editor.insertPlainText(fileText)
+        # Get name of file without rest of path
+        if platform.system() == "Windows": # Account for windows using "\" as path seperator rather than "/"
+            pathSeperator = '\\' # Double backslash to escape the first backslash
+        else:
+            pathSeperator = '/'
+
+        lastPathSepIndex = fileName[::-1].index(pathSeperator) # Reverse file path to facilitate getting index of last path seperator 
+        fileNameNoPath = ((fileName[::-1])[:lastPathSepIndex])[::-1] # Get characters of reverse path that precede last path seperator, then reverse this to get them finally in the right order
 
 
-# Setup for toolbar
-toolbar = QToolBar("Toolbar")
-toolbar.setStyleSheet("""color: white; 
-						background-color: #434343;
-						font: Source Sans Pro;""")
+        # Setup for code editor
+        editor = QTextEdit()
+        editor.setStyleSheet("""color: white; 
+                                background-color: #484D5D; 
+                                border-style: none; 
+                                font: courier new; 
+                                font-size: 10pt;""")
+        editor.setTabStopDistance(QFontMetrics(editor.font()).horizontalAdvance(' ') * 4) # Changing tab size to 4 spaces 
+        self.setCentralWidget(editor)
+        editor.insertPlainText(fileText)
 
-# Toolbar actions setup: Save, Save As & Find W/ keyboard shortcuts
-save = QAction("Save")
-save.triggered.connect(lambda: saving.save(fileName, editor.toPlainText()))
-save.setShortcut(QKeySequence("Ctrl+s"))
-toolbar.addAction(save)
-												
-saveAs = QAction("Save As")
-saveAs.triggered.connect(lambda: saving.saveAs(editor.toPlainText()))
-saveAs.setShortcut(QKeySequence("Ctrl+Shift+s"))
-toolbar.addAction(saveAs)
 
-find = QAction("Find")
-find.triggered.connect(lambda: findReplace.find())
-find.setShortcut(QKeySequence("Ctrl+f"))
-toolbar.addAction(find)
+        # Setup for toolbar
+        toolbar = QToolBar("Toolbar")
+        toolbar.setStyleSheet("""color: white; 
+                                background-color: #434343;
+                                font: Source Sans Pro;""")
 
-window.addToolBar(toolbar)
+        # Toolbar actions setup: Save, Save As & Find W/ keyboard shortcuts
+        save = QAction("Save")
+        save.triggered.connect(lambda: saving.save(fileName, editor.toPlainText()))
+        save.setShortcut(QKeySequence("Ctrl+s"))
+        toolbar.addAction(save)
+                                                        
+        saveAs = QAction("Save As")
+        saveAs.triggered.connect(lambda: saving.saveAs(editor.toPlainText()))
+        saveAs.setShortcut(QKeySequence("Ctrl+Shift+s"))
+        toolbar.addAction(saveAs)
 
-																		
+        find = QAction("Find")
+        find.triggered.connect(lambda: findReplace.find(editor.toPlainText(), self.centralWidget()))
+        find.setShortcut(QKeySequence("Ctrl+f"))
+        toolbar.addAction(find)
+
+        self.addToolBar(toolbar)
+
+
+window = Window()
 window.show()
 
 sys.exit(app.exec())
