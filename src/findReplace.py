e1a168774405e77482685e87ae323b0c8355f2fc
Devin Sidhu <devinsidhu24@gmail.com>
2022-11-26 13:43:17 +0000

    Refactored __replace() function so that it now uses cursors to refer to instances rather than their indexes. Thus, the self.instances array now contains QTextCursors that select the instance rather than 2D arrays representing the indexes of an instance

--- src/findReplace.py
+++ src/findReplace.py
@@ -23,12 +23,9 @@ class FindReplacePopup(QDialog):
                             font: Garet;""")
 
         self.editor = editor 
+        self.document = editor.document()
 
-        self.instances = [] # 2D array - Stores positions of the start & end of all found text instances in form: [start, end] 
-
-        # Note: Because each subarray of self.instances[] is designed to hold the anchor (start) and position (end) of a hypothetical QTextCursor that selects the instance, 
-        # the second element of the subarray (i.e the "end") will be 1 + the position of the final character of the instance within the document.
-        # This is because a QTextCursor positions itself BETWEEN characters, so the end of the cursor's selection is between the last character of the instance and the following character
+        self.instances = [] # Array of QTextCursors - stores the cursors that select individual occurences of found text.
 
         layout = QGridLayout()
 
@@ -97,32 +94,36 @@ class FindReplacePopup(QDialog):
         if searchTerm == "":
             return
 
-        document = self.editor.document()
+        document = self.document
+        text = document.toPlainText()
 
-        self.__unhighlight() # Remove previous highlightings from the document that were created by a previous function call.
+        self.__unhighlight() # Remove previous highlights from the document that were created by a previous call to this function.
         self.instances = [] # Clear instances array of any instances left over from a previous function call.
 
-        text = document.toPlainText()
-        position = [] # Represents one instance of search term. Stores the position of initial & final character
         isDiff = False # Flag indicating whether text being examined is discrepant from searchTerm
 
         for i in range(len(text)):
                 
-                if text[i] == searchTerm[0]:
+                if text[i] == searchTerm[0]: # Look for first character of searchTerm
 
-                    if len(searchTerm) > 1: # As we have established that the first characters are equal, if the searchTerm only has 1 character then we can conclude the scanning of this particular instance.
-
+                    if len(searchTerm) == 1: # As we have established that the first characters are equal, if the searchTerm only has 1 character then we can conclude the scanning of this particular instance.
+                        offset = 0 # There is no offset if searchTerm only has 1 character.
+                    
+                    else:
                         for offset in range(1, len(searchTerm)): # Range starts at 1, not 0, as we have already established that the first characters are equal.
                             if searchTerm[offset] != text[i + offset]:
                                 isDiff = True
                                 break
-                    
-                    else:
-                        offset = 0 # There is no offset if searchTerm only has 1 character.
-                
+                        
                     if not isDiff:
-                        position = [i, i + offset + 1]  # Note that because QTextCursor positions itself between characters, the cursors final position must be offset + 1.
-                        self.instances.append(position)
+
+                        start = i 
+                        end = i + offset + 1  # Note that because QTextCursor positions itself between characters, the cursors final position must be offset + 1.
+
+                        cursor = QTextCursor(self.document)
+                        cursor.setPosition(start, QTextCursor.MoveMode.MoveAnchor) # Navigate cursor to instance.
+                        cursor.setPosition(end, QTextCursor.MoveMode.KeepAnchor) # Select whole instance by moving position to end of instance but maintaining anchor at beginning.
+                        self.instances.append(cursor)
 
                     isDiff = False # Reset difference flag.
 
@@ -136,25 +137,21 @@ class FindReplacePopup(QDialog):
 
             highlightFmt = QTextCharFormat() 
             highlightFmt.setBackground(QColor("#40d4db"))
-            cursor = QTextCursor(document)
 
-            for instance in self.instances:
-                cursor.setPosition(instance[0], QTextCursor.MoveMode.MoveAnchor) # Navigate cursor to instance
-                cursor.setPosition(instance[1], QTextCursor.MoveMode.KeepAnchor) # Select whole instance by moving position to end of instance but maintaining anchor at beginning.
-                cursor.setCharFormat(highlightFmt) # Apply highlighting
-                if instance == self.instances[0]: # Have user's cursor select first instance
-                    self.editor.setTextCursor(cursor) 
-            
+            for instanceCursor in self.instances:
+                instanceCursor.setCharFormat(highlightFmt) # Apply highlighting
+
+            self.editor.setTextCursor(self.instances[0]) # Have user's cursor select first instance
+        
 
     # Removes highlighting from document that was created by __find(). 
     def __unhighlight(self):
 
-        document = self.editor.document()
         defaultFmt = QTextCharFormat() 
         defaultFmt.setBackground(QColor("#0E0E10")) # Background will be reset to the background color of the editor 
-        cursor = QTextCursor(document)
+        cursor = QTextCursor(self.document)
 
-        cursor.setPosition(len(document.toPlainText()), QTextCursor.MoveMode.KeepAnchor) # Select entire document
+        cursor.setPosition(len(self.document.toPlainText()), QTextCursor.MoveMode.KeepAnchor) # Select entire document
         cursor.setCharFormat(defaultFmt)
 
 
@@ -165,21 +162,16 @@ class FindReplacePopup(QDialog):
             return
 
         currentCursor = self.editor.textCursor()
-        newCursor = QTextCursor(self.editor.document())
+        newCursor = QTextCursor(self.document)
 
-        for i in range(len(self.instances)): # Find instance which user's current cursor is at
+        for i in range(len(self.instances)): # Find instance which user's current cursor is at.
 
-            if self.instances[i][0] == currentCursor.anchor():
+            if self.instances[i] == currentCursor:
 
                 try:
-                    newCursor.setPosition((self.instances)[i + 1][0], QTextCursor.MoveMode.MoveAnchor) # Navigate new cursor to next instance from the instance user's cursor is at
-                    newCursor.setPosition((self.instances)[i + 1][1], QTextCursor.MoveMode.KeepAnchor) # Select whole instance by moving position to end of instance but maintaining anchor at beginning.
-                    break
-                    # Note that because the cursor positions itself between characters, the cursors final position must be the final character's position + 1.
-
-                except IndexError: # An IndexError will be raised from the above statement if there is no instance after the currently selected one. In this case we should put the cursor back to the first instance.
-                    newCursor.setPosition((self.instances)[0][0], QTextCursor.MoveMode.MoveAnchor) 
-                    newCursor.setPosition((self.instances)[0][1], QTextCursor.MoveMode.KeepAnchor) 
+                    newCursor = self.instances[i + 1]
+                except IndexError: # Raised if there is no instance after the currently selected one. In this case, put the cursor back to the first instance.
+                    newCursor = self.instances[0]
                     
         self.editor.setTextCursor(newCursor) 
 
@@ -191,80 +183,49 @@ class FindReplacePopup(QDialog):
             return
 
         currentCursor = self.editor.textCursor()
-        newCursor = QTextCursor(self.editor.document())
+        newCursor = QTextCursor(self.document)
 
-        for i in range(len(self.instances)): # Find instance which user's current cursor is at
+        for i in range(len(self.instances)): # Find instance which user's current cursor is at.
 
-            if self.instances[i][0] == currentCursor.anchor():
+            if self.instances[i] == currentCursor:
 
                 try:
-                    newCursor.setPosition(self.instances[i - 1][0], QTextCursor.MoveMode.MoveAnchor) # Navigate new cursor to next instance from the instance user's cursor is at
-                    newCursor.setPosition(self.instances[i - 1][1], QTextCursor.MoveMode.KeepAnchor) # Select whole instance by moving position to end of instance but maintaining anchor at beginning.
-                    # Note that because the cursor positions itself between characters, the cursors final position must be the final character's position + 1.
-                    break
-
-                except IndexError: # An IndexError will be raised from the above statement if there is no instance before the currently selected one. In this case we should put the cursor to the last instance.
-                    newCursor.setPosition(self.instances[len(self.instances)][0], QTextCursor.MoveMode.MoveAnchor) 
-                    newCursor.setPosition(self.instances[len(self.instances)][1], QTextCursor.MoveMode.KeepAnchor) 
+                    newCursor = self.instances[i - 1]
+                except IndexError: # Raised if there is no instance before the currently selected one. In this case, put the cursor to the last instance.
+                    newCursor = self.instances[len(self.instances) - 1]
                     
         self.editor.setTextCursor(newCursor) 
 
 
     """
-    Replace instance of found text on which the user's cursor is positioned with new text, then move user's cursor to next instance.
+    Replace instance of found text on which the user's cursor is positioned with new text, 
+    moving the user's cursor to next instance in the process.
 
         PARAMETERS:
             newText - The text to replace the selected instance with.
     """
     def __replace(self, newText):
-        
-        # Exit function if no text was entered
-        if newText == "":
-            return
 
-        # This means that the user has pressed the replace button without first finding any instances.
-        if self.instances == []: 
+        if newText == "" or self.instances == []:
             return
-
-        originalAnchor = self.editor.textCursor().anchor()
-        originalPos = self.editor.textCursor().position()
         
+        cursorForInstance = self.editor.textCursor()
+
         # Removing highlighting from instance.
         defaultFmt = QTextCharFormat() 
         defaultFmt.setBackground(QColor("#0E0E10")) # Background will be reset to the background color of the editor 
-        self.editor.textCursor().setCharFormat(defaultFmt)
-
-        self.editor.textCursor().insertText(newText) # insertText() also deletes current selection before inserting new text
-
-        # insertText() moves the user's cursor's anchor and position to the end of the insertion, 
-        # so we need to reset these to their previous values in order to be able to find the user's cursor in self.instances[], 
-        # which in turn is necessary for __nextInstance to work.
-        resetCursor = QTextCursor(self.editor.document())
-        resetCursor.setPosition(originalAnchor, QTextCursor.MoveMode.MoveAnchor)
-        resetCursor.setPosition(originalPos, QTextCursor.MoveMode.KeepAnchor)
-        self.editor.setTextCursor(resetCursor)
-
-        # ACCOUNTING FOR CHARACTER SHIFT:
-        #
-        # By replacing the original text with new text that is of a different length to the old text, 
-        # all characters that occur AFTER the replaced text are shifted, thus their positions in the document change
-        # The positions are shifted by the difference in length between the new and old text.
-        # Therefore it is necessary to update the positions stored in the self.instances array to account for these changes.
-        #
-        # As lengthDifference is the length of the new text minus that of the old text, it will be negative if the old text is longer than the new text. 
-        # This is not an issue, as when lengthDifference is added to the positions, if LengthDifference is negative it's absolute value will be subtracted from the positions, 
-        # shifting the characters leftwards, which is the desired effect if the new text is shorter than the old text.
-        originalLength = originalPos - originalAnchor
-        if originalLength != len(newText):
-            lengthDifference = len(newText) - originalLength
-            for i in self.instances:
-                if i[0] > self.editor.textCursor().anchor(): # Only shift instances if they occur after instance being replaced
-                    i[0] = i[0] + lengthDifference
-                    i[1] = i[1] + lengthDifference
+        cursorForInstance.setCharFormat(defaultFmt)
 
         self.__nextInstance()
-        self.instances.remove([originalAnchor, originalPos]) # Remove just-replaced instance from self.instances. 
-        
+
+        # Remove instance to be replaced from self.instances
+        for i in self.instances:
+            if i.anchor() == cursorForInstance.anchor():
+                self.instances.remove(i)
+                break
+
+        cursorForInstance.insertText(newText) # insertText() also deletes current selection before inserting new text
+
 
     """
     Replace all instances of found text in file with new text.
@@ -274,14 +235,10 @@ class FindReplacePopup(QDialog):
     """
     def __replaceAll(self, newText):
         
-        # Exit function if no text was entered
-        if newText == "":
-            return
-
-        if self.instances == []:  # This means that the user has pressed the replace button without first finding any instances.
+        if newText == "" or self.instances == []:
             return
 
         for i in range(len(self.instances)):
             self.__replace(newText)
 
-        self.__unhighlight() # Remove highlighting from document now that all instances of the original text have been replaced.
+        self.__unhighlight() # Remove all highlighting from document now that all instances have been replaced.
